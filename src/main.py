08c7d2746b47de90157ba92a863a32a7aa8ff7bc
jadenwei4 <jadenwei4@gmail.com>
2026-05-04 17:36:10 -0400

    chore: build site for github pages

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
 import os
 import shutil
+import sys
+
+print(sys.argv)
 
 from copystatic import copy_files_recursive
-from gencontent import generate_page, generate_pages_recursive
+from gencontent import generate_pages_recursive
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 dir_path_content_glorfindel = "./content/blog/glorfindel"
 dir_path_content_majesty = "./content/blog/majesty"
@@ -15,6 +18,8 @@ template_path = "./template.html"
 
 
 def main():
+    args = sys.argv
+    basepath = args[1] if args[1] else '/'
     print("Deleting public directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
@@ -23,7 +28,7 @@ def main():
     copy_files_recursive(dir_path_static, dir_path_public)
 
     print("Generating content...")
-    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
 
 
 main()
