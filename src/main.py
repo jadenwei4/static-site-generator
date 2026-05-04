eb4aef1e421855dda239af8079b1c620b88908e6
jadenwei4 <jadenwei4@gmail.com>
2026-05-04 17:24:44 -0400

    feat: big stuff

--- src/main.py
+++ src/main.py
@@ -1,9 +1,29 @@
-from textnode import TextNode, TextType
+import os
+import shutil
+
+from copystatic import copy_files_recursive
+from gencontent import generate_page, generate_pages_recursive
+
+dir_path_static = "./static"
+dir_path_public = "./public"
+dir_path_content = "./content"
+dir_path_content_glorfindel = "./content/blog/glorfindel"
+dir_path_content_majesty = "./content/blog/majesty"
+dir_path_content_tom = "./content/blog/tom"
+dir_path_content_contact = "./content/contact"
+template_path = "./template.html"
+
 
 def main():
-    test = TextNode("blah blah", TextType.BOLD)
-    test2 = TextNode("blah blah", TextType.BOLD)
-    print(test)
-    print(test == test2)
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
+
+    print("Generating content...")
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+
 
 main()
